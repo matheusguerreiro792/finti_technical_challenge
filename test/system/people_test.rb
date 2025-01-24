a4e354c8fe05d7349f32d4ad1d099f74b8ba3ef7
Matheus Guerreiro <matheus.guerreiro.792@gmail.com>
2025-01-24 05:44:42 -0300

    fix; system people_test. '

--- test/system/people_test.rb
+++ test/system/people_test.rb
@@ -7,42 +7,42 @@ class PeopleTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit people_url
-    assert_selector "h1", text: "People"
+    assert_selector "h1", text: "Pessoas Cadastradas"
   end
 
   test "should create person" do
     visit people_url
-    click_on "New person"
+    click_on "Cadastrar Nova Pessoa"
 
     fill_in "Birth date", with: @person.birth_date
     fill_in "Cell phone", with: @person.cell_phone
     fill_in "Gender", with: @person.gender
     fill_in "Last name", with: @person.last_name
     fill_in "Name", with: @person.name
-    click_on "Create Person"
+    click_on "Cadastrar"
 
     assert_text "Person was successfully created"
-    click_on "Back"
+    click_on "Voltar para Pessoas Cadastradas"
   end
 
   test "should update Person" do
     visit person_url(@person)
-    click_on "Edit this person", match: :first
+    click_on "Editar esta Pessoa", match: :first
 
     fill_in "Birth date", with: @person.birth_date
     fill_in "Cell phone", with: @person.cell_phone
     fill_in "Gender", with: @person.gender
     fill_in "Last name", with: @person.last_name
     fill_in "Name", with: @person.name
-    click_on "Update Person"
+    click_on "Editar"
 
     assert_text "Person was successfully updated"
-    click_on "Back"
+    click_on "Voltar para Pessoas Cadastradas"
   end
 
   test "should destroy Person" do
     visit person_url(@person)
-    click_on "Destroy this person", match: :first
+    click_on "Excluir esta Pessoa", match: :first
 
     assert_text "Person was successfully destroyed"
   end
