643602d7045cf795fbb60d6d810c80d9ccf2f1f6
Matheus Guerreiro <matheus.guerreiro.792@gmail.com>
2025-01-24 05:15:20 -0300

    refac; person and validates. '

---
+++ app/models/concerns/ddd_state_mapper.rb
@@ -0,0 +1,30 @@
+module DddStateMapper
+  extend ActiveSupport::Concern
+
+  included do
+    cattr_accessor :ddd_to_state
+    self.ddd_to_state = load_ddd_to_state
+  end
+
+  class_methods do
+    def load_ddd_to_state
+      YAML.load_file(Rails.root.join('config', 'ddd_to_state.yml'))['ddd_to_state']
+    rescue Errno::ENOENT, Psych::SyntaxError => e
+      Rails.logger.error("Error loading ddd_to_state.yml: #{e.message}")
+      {}
+    end
+  end
+
+  def state
+    @state ||= begin
+      ddd = cell_phone[1, 2]
+      self.class.ddd_to_state[ddd] || 'Desconhecido'
+    end
+  end
+
+  def valid_ddd
+    unless self.class.ddd_to_state.key?(cell_phone[0, 2])
+      errors.add(:cell_phone, :invalid)
+    end
+  end
+end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,11 @@
 class Person < ApplicationRecord
+  include DddStateMapper
+
   validates :name, :gender, :birth_date, :cell_phone, presence: true
+  validates :gender, inclusion: { in: %w[Masculino Feminino] }
+  validates :birth_date, comparison: { less_than_or_equal_to: Date.today, message: 'não pode ser maior que a data atual' }
   validate :cell_phone_format
+  validate :valid_ddd
 
   before_save :correct_phone_format
 
@@ -26,85 +31,6 @@ class Person < ApplicationRecord
 
   def can_vote?
     age >= 16
-  end
-
-  def state
-    ddd_to_estado = {
-    # Região Sul
-    41 => "Paraná",
-    42 => "Paraná/Santa Catarina",
-    43 => "Paraná",
-    44 => "Paraná",
-    45 => "Paraná",
-    46 => "Paraná",
-    47 => "Santa Catarina",
-    48 => "Santa Catarina",
-    49 => "Santa Catarina/Paraná",
-    51 => "Rio Grande do Sul",
-    53 => "Rio Grande do Sul",
-    54 => "Rio Grande do Sul",
-    55 => "Rio Grande do Sul",
-    # Região Nordeste
-    71 => "Bahia",
-    73 => "Bahia",
-    74 => "Bahia",
-    75 => "Bahia",
-    77 => "Bahia",
-    79 => "Sergipe",
-    81 => "Pernambuco",
-    82 => "Alagoas",
-    83 => "Paraíba",
-    84 => "Rio Grande do Norte",
-    85 => "Ceará",
-    86 => "Piauí",
-    87 => "Pernambuco",
-    88 => "Ceará",
-    89 => "Piauí",
-    98 => "Maranhão",
-    99 => "Maranhão",
-    # Região Norte
-    63 => "Tocantins",
-    68 => "Acre",
-    69 => "Rondônia",
-    91 => "Pará",
-    92 => "Amazonas",
-    93 => "Pará",
-    94 => "Pará",
-    95 => "Roraima",
-    96 => "Amapá",
-    97 => "Amazonas",
-    # Região Sudeste
-    11 => "São Paulo",
-    12 => "São Paulo",
-    13 => "São Paulo",
-    14 => "São Paulo",
-    15 => "São Paulo",
-    16 => "São Paulo",
-    17 => "São Paulo",
-    18 => "São Paulo",
-    19 => "São Paulo",
-    21 => "Rio de Janeiro",
-    22 => "Rio de Janeiro",
-    24 => "Rio de Janeiro",
-    27 => "Espírito Santo",
-    28 => "Espírito Santo",
-    31 => "Minas Gerais",
-    32 => "Minas Gerais",
-    33 => "Minas Gerais",
-    34 => "Minas Gerais",
-    35 => "Minas Gerais",
-    37 => "Minas Gerais",
-    38 => "Minas Gerais",
-    # Região Centro-Oeste
-    61 => "Distrito Federal/Goiás",
-    62 => "Goiás",
-    64 => "Goiás",
-    65 => "Mato Grosso",
-    66 => "Mato Grosso",
-    67 => "Mato Grosso do Sul"
-  }
-
-    ddd_to_estado[cell_phone[1..2].to_i]
   end
 
   def have_peoples?
